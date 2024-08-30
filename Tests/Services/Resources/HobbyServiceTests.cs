99d4d4bf9283bf534db826ef19bb06dc3b276f67
Ronnie Hartmann <ronnie.hartmann@noser.com>
2024-08-30 12:48:50 +0200

    Add more tests

---
+++ Tests/Services/Resources/HobbyServiceTests.cs
@@ -0,0 +1,115 @@
+﻿using Application.Dtos;
+using Application.Services.Resources;
+using Application.Services.Resources.Impl;
+using Domain.Models;
+using Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Tests.DummyData;
+
+namespace Tests.Services.Resources;
+
+public class HobbyServiceTests
+{
+    private ResourceService<HobbyDto> _hobbyService;
+    private Mock<IHobbyRepository> _hobbyRepositoryMock;
+    private Mock<ILogger<HobbyService>> _loggerMock;
+
+    [SetUp]
+    public void Setup()
+    {
+        _hobbyRepositoryMock = new Mock<IHobbyRepository>();
+        _loggerMock = new Mock<ILogger<HobbyService>>();
+        _hobbyService = new HobbyService(_hobbyRepositoryMock.Object, _loggerMock.Object);
+    }
+
+    [Test]
+    public async Task GetResourcesAsync_HasResources_Success()
+    {
+        // Arrange
+        _hobbyRepositoryMock.Setup(x => x.GetAllHobbiesAsync()).ReturnsAsync(HobbyData.DEFAULT_HOBBIES);
+
+        // Act
+        var resources = await _hobbyService.GetResourcesAsync();
+
+        // Assert
+        Assert.That(resources, Has.Exactly(2).Items);
+        Assert.That(resources, Has.One.Matches<HobbyDto>(x => string.Equals(x.Name, "Skateboarding")));
+    }
+
+    [Test]
+    public async Task AddResourceAsync_Resource_Success()
+    {
+        // Arrange
+        _hobbyRepositoryMock.Setup(x => x.GetAllHobbiesAsync()).ReturnsAsync([]);
+
+        // Act
+        await _hobbyService.AddResourceAsync(HobbyData.DEFAULT_HOBBY_DTO);
+
+        // Assert
+        _hobbyRepositoryMock.Verify(x => x.AddHobbyAsync(It.IsAny<Hobby>()), Times.Once);
+    }
+
+    [Test]
+    public void AddResourceAsync_Null_ArgumentNullException()
+    {
+        // Arrange
+        _hobbyRepositoryMock.Setup(x => x.GetAllHobbiesAsync()).ReturnsAsync([]);
+
+        // Act & Assert
+        Assert.That(async () => await _hobbyService.AddResourceAsync(null!), Throws.TypeOf<ArgumentNullException>());
+        _hobbyRepositoryMock.Verify(x => x.AddHobbyAsync(It.IsAny<Hobby>()), Times.Never);
+    }
+
+    [Test]
+    public async Task UpdateResourceAsync_Resource_Success()
+    {
+        // Arrange
+        _hobbyRepositoryMock.Setup(x => x.GetHobbyAsync(It.IsAny<long>())).ReturnsAsync(HobbyData.DEFAULT_HOBBIES.First());
+        var resourceToUpdate = HobbyData.DEFAULT_HOBBY_DTO;
+        resourceToUpdate.Name = "Other Hobby";
+
+        // Act
+        await _hobbyService.UpdateResourceAsync(resourceToUpdate);
+
+        // Assert
+        _hobbyRepositoryMock.Verify(x => x.UpdateHobbyAsync(It.IsAny<Hobby>()), Times.Once);
+    }
+
+    [Test]
+    public void UpdateResourceAsync_Null_ArgumentNullException()
+    {
+        // Act & Assert
+        Assert.That(async () => await _hobbyService.UpdateResourceAsync(null!), Throws.TypeOf<ArgumentNullException>());
+        _hobbyRepositoryMock.Verify(x => x.UpdateHobbyAsync(It.IsAny<Hobby>()), Times.Never);
+    }
+
+    [Test]
+    public void UpdateResourceAsync_MissingEducation_InvalidOperationException()
+    {
+        // Arrange
+        _hobbyRepositoryMock.Setup(x => x.GetHobbyAsync(It.IsAny<long>())).ReturnsAsync((Hobby)null!);
+
+        // Act & Assert
+        Assert.That(async () => await _hobbyService.UpdateResourceAsync(HobbyData.DEFAULT_HOBBY_DTO), Throws.TypeOf<InvalidOperationException>());
+        _hobbyRepositoryMock.Verify(x => x.UpdateHobbyAsync(It.IsAny<Hobby>()), Times.Never);
+    }
+
+    [Test]
+    public async Task DeleteResourceAsync_ValidId_Success()
+    {
+        // Act
+        await _hobbyService.DeleteResourceAsync(1);
+
+        // Assert
+        _hobbyRepositoryMock.Verify(x => x.DeleteHobbyAsync(It.IsAny<long>()), Times.Once);
+    }
+
+    [Test]
+    public void DeleteResourceAsync_NegativeId_ArgumentException()
+    {
+        // Act & Assert
+        Assert.That(async () => await _hobbyService.DeleteResourceAsync(-1), Throws.TypeOf<ArgumentException>());
+        _hobbyRepositoryMock.Verify(x => x.DeleteHobbyAsync(It.IsAny<long>()), Times.Never);
+    }
+}
---
+++ Tests/Services/Resources/SkillServiceTests.cs
@@ -0,0 +1,115 @@
+﻿using Application.Dtos;
+using Application.Services.Resources;
+using Application.Services.Resources.Impl;
+using Domain.Models;
+using Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Tests.DummyData;
+
+namespace Tests.Services.Resources;
+
+public class SkillServiceTests
+{
+    private ResourceService<SkillDto> _skillService;
+    private Mock<ISkillRepository> _skillRepositoryMock;
+    private Mock<ILogger<SkillService>> _loggerMock;
+
+    [SetUp]
+    public void Setup()
+    {
+        _skillRepositoryMock = new Mock<ISkillRepository>();
+        _loggerMock = new Mock<ILogger<SkillService>>();
+        _skillService = new SkillService(_skillRepositoryMock.Object, _loggerMock.Object);
+    }
+
+    [Test]
+    public async Task GetResourcesAsync_HasResources_Success()
+    {
+        // Arrange
+        _skillRepositoryMock.Setup(x => x.GetAllSkillsAsync()).ReturnsAsync(SkillData.DEFAULT_SKILLS);
+
+        // Act
+        var resources = await _skillService.GetResourcesAsync();
+
+        // Assert
+        Assert.That(resources, Has.Exactly(2).Items);
+        Assert.That(resources, Has.One.Matches<SkillDto>(x => string.Equals(x.Name, "C#")));
+    }
+
+    [Test]
+    public async Task AddResourceAsync_Resource_Success()
+    {
+        // Arrange
+        _skillRepositoryMock.Setup(x => x.GetAllSkillsAsync()).ReturnsAsync([]);
+
+        // Act
+        await _skillService.AddResourceAsync(SkillData.DEFAULT_SKILL_DTO);
+
+        // Assert
+        _skillRepositoryMock.Verify(x => x.AddSkillAsync(It.IsAny<Skill>()), Times.Once);
+    }
+
+    [Test]
+    public void AddResourceAsync_Null_ArgumentNullException()
+    {
+        // Arrange
+        _skillRepositoryMock.Setup(x => x.GetAllSkillsAsync()).ReturnsAsync([]);
+
+        // Act & Assert
+        Assert.That(async () => await _skillService.AddResourceAsync(null!), Throws.TypeOf<ArgumentNullException>());
+        _skillRepositoryMock.Verify(x => x.AddSkillAsync(It.IsAny<Skill>()), Times.Never);
+    }
+
+    [Test]
+    public async Task UpdateResourceAsync_Resource_Success()
+    {
+        // Arrange
+        _skillRepositoryMock.Setup(x => x.GetSkillAsync(It.IsAny<long>())).ReturnsAsync(SkillData.DEFAULT_SKILLS.First());
+        var resourceToUpdate = SkillData.DEFAULT_SKILL_DTO;
+        resourceToUpdate.Name = ".NET";
+
+        // Act
+        await _skillService.UpdateResourceAsync(resourceToUpdate);
+
+        // Assert
+        _skillRepositoryMock.Verify(x => x.UpdateSkillAsync(It.IsAny<Skill>()), Times.Once);
+    }
+
+    [Test]
+    public void UpdateResourceAsync_Null_ArgumentNullException()
+    {
+        // Act & Assert
+        Assert.That(async () => await _skillService.UpdateResourceAsync(null!), Throws.TypeOf<ArgumentNullException>());
+        _skillRepositoryMock.Verify(x => x.UpdateSkillAsync(It.IsAny<Skill>()), Times.Never);
+    }
+
+    [Test]
+    public void UpdateResourceAsync_MissingEducation_InvalidOperationException()
+    {
+        // Arrange
+        _skillRepositoryMock.Setup(x => x.GetSkillAsync(It.IsAny<long>())).ReturnsAsync((Skill)null!);
+
+        // Act & Assert
+        Assert.That(async () => await _skillService.UpdateResourceAsync(SkillData.DEFAULT_SKILL_DTO), Throws.TypeOf<InvalidOperationException>());
+        _skillRepositoryMock.Verify(x => x.UpdateSkillAsync(It.IsAny<Skill>()), Times.Never);
+    }
+
+    [Test]
+    public async Task DeleteResourceAsync_ValidId_Success()
+    {
+        // Act
+        await _skillService.DeleteResourceAsync(1);
+
+        // Assert
+        _skillRepositoryMock.Verify(x => x.DeleteSkillAsync(It.IsAny<long>()), Times.Once);
+    }
+
+    [Test]
+    public void DeleteResourceAsync_NegativeId_ArgumentException()
+    {
+        // Act & Assert
+        Assert.That(async () => await _skillService.DeleteResourceAsync(-1), Throws.TypeOf<ArgumentException>());
+        _skillRepositoryMock.Verify(x => x.DeleteSkillAsync(It.IsAny<long>()), Times.Never);
+    }
+}
